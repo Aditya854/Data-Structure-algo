2fb7d58adb5fafe7c681d9073425f93365bb0d49
Aditya gupta <guptaaditya854@gmail.com>
2023-09-05 08:12:33 +0530

    .

--- OA2.cpp
+++ OA2.cpp
@@ -1,28 +1,45 @@
-#include <iostream>
-#include <unordered_set>
-#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 
-std::vector<int> removeDuplicates(const std::vector<int>& input) {
-    std::unordered_set<int> seen;
-    std::vector<int> result;
 
-    for (int num : input) {
-        if (seen.find(num) == seen.end()) {
-            seen.insert(num);
-            result.push_back(num);
-        }
-    }
+vector<bool> prime;
+void sieve(int x, int y)
+{
 
-    return result;
+	prime.resize(y + 1, true);
+	prime[1] = false;
+
+	for (int i = 2; i * i <= y; i++) {
+		if (prime[i])
+			for (int j = i * i; j <= y; j += i) {
+				prime[j] = false;
+			}
+	}
 }
 
-int main() {
-    std::vector<int> input = {1,1,3,2,1,4,5,4};
-    std::vector<int> uniqueNumbers = removeDuplicates(input);
+vector<int> allPrime(int l, int r)
+{
+	vector<int> result;
+	for (int i = l; i <= r; i++) {
+		if (prime[i]) {
+			result.push_back(i);
+		}
+	}
 
-    for (int num : uniqueNumbers) {
-        std::cout << num << " ";
-    }
+	return result;
+}
 
-    return 0;
+int main()
+{
+	int starting_range = 23;
+	int ending_range = 42;
+
+	sieve(starting_range, ending_range);
+
+	vector<int> result
+		= allPrime(starting_range, ending_range);
+
+	int ans = result.size();
+    cout<<ans<<endl;
+	return 0;
 }
