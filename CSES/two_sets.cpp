d5d8101d9bb41568dc1130b7858756cb34a19b02
Aditya gupta <guptaaditya854@gmail.com>
2025-01-06 23:21:17 +0530

    .

---
+++ CSES/two_sets.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+void solve() {
+ int n;
+ cin>>n;
+ if((n*(n+1))%4==0)
+ {
+    cout<<"YES"<<endl;
+ }
+ else{
+    cout<<"NO"<<endl;
+ }
+ return;
+}
+
+
+int main()
+{
+    solve();
+    return 0;
+}
